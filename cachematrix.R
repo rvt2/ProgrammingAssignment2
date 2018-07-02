1be974eeb5cfcfd26eb5c0e51831ee32400e9f63
rvt2 <40119256+rvt2@users.noreply.github.com>
2018-07-02 02:46:34 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix 
 
-## Write a short comment describing this function
+# makeCacheMatric builds a set of four functions, set(), get(), setinv(), getinv()
+# Descriptions for each are inline with the function
+# These store and retrieve a matrix and inverse matrix in conjunture with cacheSolve.R
+# They are stored as a list to be returned to the parent environment
+#
+# After reading the tutorial it was clear the most efficient solution was to just modify the assignment code. 
 
 makeCacheMatrix <- function(x = matrix()) {   
   i <- NULL                        #obj to store inverse matrix
                                    #this will be available for all makeCacheMatrix() functions 
-  
   set <- function(y) {
     x <<- y                        #assign input argument into x in the parent environment because of <<- operator
-    i <<- NULL                     #reset the matrix i, if set() is called we should overwrite 
-                                   #whatever cacheSolve has already set in i
+    i <<- NULL                     #reset the matrix i, if set() is called we should overwrite what cacheSolve has already set in i
   }                               
-  get <- function() x              #x is retrieved from parent env, not local to get()
+  get <- function() {
+    x                              #x is retrieved from parent env, not local to get()
+  }
   
-  setinv <- function(inv) i <<- inv #no value until called from cacheSolve
-                                    #<<- is used so i is updated in the parent env. and can be used in other functions.
+  setinv <- function(inv) {
+    i <<- inv                      #no value until called from cacheSolve
+  }                                #<<- is used so i is updated in the parent env. and can be used in other functions.
   
-  
-  getinv <- function() i          #
-  
-  list(set = set, get = get,      #store the 4 makeCacheMatrix functions in a list
-       setinv = setinv,           #so when the function is stored in an obj. they can be retrieved. 
-       getinv = getinv)
+  getinv <- function() {
+    i                              #return what is in i if called
+  }
+    
+  list(set = set, get = get,       #store the 4 makeCacheMatrix functions in a list
+       setinv = setinv,            #so when the function is stored in an obj. they can be retrieved with the $ form 
+       getinv = getinv)            #of the extract operator
 }
+
 
 
 ## Write a short comment describing this function
